0f15cf3b5d07a3a2355282b829d9cd3f0ba06b5e
Rhuan-Rodrigues1 <gitrhuan34@gmail.com>
2023-08-29 14:17:29 -0300

    remove in tree

--- árvores/main.c
+++ árvores/main.c
@@ -44,6 +44,61 @@ void imprimir(Node *n) {
     imprimir(n->direita);
 }
 
+void remover(Node *n, int valor) {
+    Node *filho = n;
+    Node *pai;
+
+    do
+    {
+        pai = filho;
+
+        if (valor < filho->valor)
+        {
+            filho = filho->esquerda;
+        } else if (valor > filho->valor) {
+            filho = filho->direita;
+        }
+        
+    } while (filho != NULL && filho->valor != valor);
+
+    if(filho != NULL) {
+        if(filho->esquerda == NULL && filho->direita == NULL) {
+            if(pai->esquerda == filho) pai->esquerda = NULL;
+            if(pai->direita == filho) pai->direita = NULL;
+        }
+
+        if(filho->esquerda != NULL && filho->direita == NULL) {
+            if(pai->esquerda == filho) pai->esquerda = filho->esquerda;
+            if(pai->direita == filho) pai->direita = filho->esquerda;
+        }
+
+        if(filho->direita != NULL && filho->esquerda == NULL) {
+            if(pai->esquerda == filho) pai->esquerda = filho->direita;
+            if(pai->direita == filho) pai->direita = filho->direita;
+        }
+
+        if(filho->esquerda != NULL && filho->direita != NULL) {
+            if(filho->esquerda->direita == NULL) {
+                filho->valor = filho->esquerda->valor;
+                filho->esquerda = NULL;
+            } else {
+                Node *p = filho->esquerda;
+                Node *aux = p;
+
+                while(p->direita != NULL) {
+                    aux = p;
+                    p = p->direita;
+                }
+
+                aux->direita = NULL;
+                filho->valor = p->valor;
+
+            }
+        }
+    }
+    
+}
+
 int main(void) {
 
     Node *root = criar(5);
@@ -53,6 +108,8 @@ int main(void) {
 	adicionar(root,1);
 	adicionar(root,8);
 	adicionar(root,4);
+
+    remover(root, 4);
 
     imprimir(root);
     return 0;
